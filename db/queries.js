675d5fd139af0112dc48dbe39793da2c77b3c7f9
Ahmed O <amo.1980@gmail.com>
2025-02-12 20:59:01 +0300

    CRUD queries

--- db/queries.js
+++ db/queries.js
@@ -1,21 +1,52 @@
 import pool from "./pool.js";
 
-export async function searchUsernames(query) {
-  const { rows } = await pool.query(
-    "SELECT * FROM usernames WHERE username LIKE ($1)",
-    ['%'+query+'%']
+//read categories
+export async function getAllCategories() {
+  const { rows } = await pool.query("SELECT * FROM categories");
+  return rows;
+}
+
+//read items
+export async function getAllItems() {
+  const { rows } = await pool.query("SELECT * FROM items");
+  return rows;
+}
+
+//create categories
+export async function addCategory(name) {
+  await pool.query("INSERT INTO categories (name) values ($1)", [name]);
+}
+
+//create items
+export async function addItem(name, make, price, quantity, cateogry) {
+  await pool.query(
+    "INSERT INTO items (name,make,price,quantity,category) values ($1,$2,$3,$4,$5)",
+    [name, make, price, quantity, cateogry]
   );
-  return rows;
-}
-export async function getAllUsernames() {
-  const { rows } = await pool.query("SELECT * FROM usernames");
-  return rows;
 }
 
-export async function insertUsername(username) {
-  await pool.query("INSERT INTO usernames (username) VALUES ($1)", [username]);
+//update category
+export async function updateCategory(id, name) {
+  await pool.query("UPDATE categories SET name = $1 where id = $2", [name, id]);
 }
 
-export async function deleteUsername(id) {
-  await pool.query("DELETE FROM usernames WHERE id=($1)", [id]);
+//update item
+export async function updateItem(id, name, make, price, quantity, cateogry) {
+  await pool.query(
+    "UPDATE  items set name = $1, make= $2,price = $3,quantity = $4,category = $5 where id = $6",
+    [name, make, price, quantity, cateogry, id]
+  );
+}
+
+//delete cateogry
+
+export async function deleteCategory(id) {
+  await pool.query("DELETE FROM items WHERE category = $1", [id]);
+  await pool.query("DELETE FROM categories WHERE id = $1", [id]);
+}
+
+//delete item
+
+export async function deleteItem(id) {
+  await pool.query("DELETE FROM items WHERE id = $1", [id]);
 }
