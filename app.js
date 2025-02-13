7446bb4fb834f338d7406301e5f17f5e62c2eb2d
Ahmed O <amo.1980@gmail.com>
2025-02-13 21:51:33 +0300

    basic html and read operation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import morgan from 'morgan'
 import path from 'path'
+import readRouter from './routers/read.js'
 
 // import rootRouter from './routes/root.js'
 // import newRouter from './routes/new.js'
@@ -14,9 +15,9 @@ app.set('view engine', 'ejs');
 app.use(morgan('dev'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/static', express.static(path.join(__dirname, 'public')));
+// app.use('/static', express.static(path.join(__dirname, 'public')));
 
-// app.use('/',rootRouter)
+app.use('/',readRouter)
 // app.use('/new',newRouter)
 // app.use('/delete',deleteRouter)
 
