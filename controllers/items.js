8339482810a95884ae441f179110c45eb3c2f064
Ahmed O <amo.1980@gmail.com>
2025-02-14 22:04:31 +0300

    more css

--- controllers/items.js
+++ controllers/items.js
@@ -2,13 +2,12 @@ import { getAllItemsForCategoryDB } from "../db/queries.js";
 
 
 export async function getAllItemsForCategory(req, res) {
-  console.log(req.params);
   const { categoryID } = req.params;
   let itemsResult;
   if (categoryID) {
     itemsResult = await getAllItemsForCategoryDB(Number(categoryID));
     res.locals.itemsResult = itemsResult;
-    console.log(itemsResult);
+    // console.log(itemsResult);
 
   }
   res.status(200).render("index");
