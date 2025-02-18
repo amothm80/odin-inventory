97c1813f49e1e2fbe63e879310a4202fd415ca0f
Ahmed O <amo.1980@gmail.com>
2025-02-18 19:21:11 +0300

    item fields schema validatior

--- controllers/items.js
+++ controllers/items.js
@@ -1,24 +1,68 @@
-import { getAllItemsForCategoryDB,getItemByIdDB } from "../db/queries.js";
+import { getAllItemsForCategoryDB, getItemByIdDB } from "../db/queries.js";
+import { validationResult, matchedData, checkSchema } from "express-validator";
 
-export async function getItemById(req,res,next){
+export const checkCategorySchema = checkSchema({
+  categoryName: {
+    errorMessage: "Invalid category",
+    escape: true,
+    trim: true,
+    notEmpty: true,
+  },
+});
+
+export const checkItemSchema = checkSchema({
+  itemName: {
+    errorMessage: "Invalid item name",
+    escape: true,
+    trim: true,
+    notEmpty: true,
+  },
+  itemMake: {
+    errorMessage: "Invalid item name",
+    escape: true,
+    trim: true,
+    notEmpty: true,
+  },
+  itemPrice: {
+    errorMessage: "Invalid item price",
+    escape: true,
+    trim: true,
+    notEmpty: true,
+    isCurrency: {
+      options: {
+        require_symbol: false,
+        allow_negatives: false,
+        allow_decimal: true,
+      },
+    },
+  },
+  itemQuantity: {
+    errorMessage: "Invalid item price",
+    escape: true,
+    trim: true,
+    notEmpty: true,
+    isNumeric: true
+  }
+});
+
+export async function getItemById(req, res, next) {
   const { itemId } = req.params;
   let itemResult;
-  if (itemId){
-    itemResult = await getItemByIdDB(Number(itemId))
+  if (itemId) {
+    itemResult = await getItemByIdDB(Number(itemId));
     res.locals.itemResult = itemResult;
   }
-  next()
+  next();
 }
 
 export async function getAllItemsForCategory(req, res) {
-  console.log('get all items')
+  console.log("get all items");
   const { categoryID } = req.params;
   let itemsResult;
   if (categoryID) {
     itemsResult = await getAllItemsForCategoryDB(Number(categoryID));
     res.locals.itemsResult = itemsResult;
     // console.log(itemsResult);
-
   }
   res.status(200).render("index");
 }
